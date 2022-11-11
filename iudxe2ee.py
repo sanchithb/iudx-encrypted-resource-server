0b9ef34090c5c85c70e19f679e412d84d4a9200b
sanchithb <55482452+sanchithb@users.noreply.github.com>
2022-11-11 14:35:04 +0530

    Update iudxe2ee.py

--- iudxe2ee.py
+++ iudxe2ee.py
@@ -10,10 +10,17 @@ from iudx.rs.ResourceResult import ResourceResult
 
 from iudx.auth.Token import Token
 
-import multiprocessing
+private_key = PrivateKey.generate()
 
-temp_key_store = {}
+b_public_key = private_key.public_key.__bytes__()
 
+# Encode the public key to base64
+
+b64_b_public_key = base64.urlsafe_b64encode(b_public_key)
+
+# Convert the encoded public key to string to send over http
+
+str_b_public_key = b64_b_public_key.decode("utf-8")
 
 class EncryptedResourceServer(ResourceServer):
     
@@ -27,20 +34,26 @@ class EncryptedResourceServer(ResourceServer):
 
         self.public_key = temp_key_store[1]
     
-        self.headers[publickey]
-
-
-    def key_gen():
-        private_key = PrivateKey.generate()
-
-        public_key = private_key.public_key
-
-        return private_key, public_key
-
+        self.headers[str_b_public_key]
+        
     def get_latest(self, queries: List[ResourceQuery]) -> List[ResourceResult]:
 
-        super().get_latest(queries)
+        ers_results = super().get_latest(queries)
 
-        ers_results = self.rs_results
+        encrypted_data = ers_results[0].results
 
+        # Decode the encrypted message from base64
+        bytes_encrypted_data = base64.urlsafe_b64decode(encrypted_data)
 
+        # Create a SealedBox object using Private Key
+        unseal_box = SealedBox(private_key)
+
+        # Decrypt the message by unsealed_box.decrypt()
+        bytes_decrypted_data = unseal_box.decrypt(bytes_encrypted_data)
+
+        # Decode to JSON format
+        decrypted_data = json.loads(bytes_decrypted_data.decode('utf-8'))
+
+        ers_results[0].results = decrypted_data
+
+        return ers_results
