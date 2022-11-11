23d5f6f68cd052e8b2c0b308f91f25c90b62ed22
sanchithb <55482452+sanchithb@users.noreply.github.com>
2022-11-11 13:48:38 +0530

    Update iudx-encrypted-resource-server.py

--- iudx-encrypted-resource-server.py
+++ iudx-encrypted-resource-server.py
@@ -6,15 +6,19 @@ from iudx.rs.ResourceQuery import ResourceQuery
 # entity id for the pune env aqm sensor.
 entity_id = "datakaveri.org/04a15c9960ffda227e9546f3f46e629e1fe4132b/rs.iudx.org.in/pune-env-aqm/f36b4669-628b-ad93-9970-f9d424afbf75"
 
-sent_key = open('publickey.key', 'r').read()
-
 # creating an object of ResourceServer class using rs_url.
-ers = EncryptedResourceServer.ers(
+ers = EncryptedResourceServer(
          rs_url="https://rs.iudx.org.in/ngsi-ld/v1",
-         headers={"content-type": "application/json"},
-         key=sent_key
+         headers={"content-type": "application/json"}
      )
 
+# creating a query for fetching latest data for the entity_id.
+rs_query = ResourceQuery()
+rs_entity = rs_query.add_entity(entity_id)
+
+# fetch results for a list of entities.
+results = ers.get_latest([rs_entity])
+
 # printing results
-print(f"RESULTS: {ers[0]}")        # get the result data of the resource query.
-print(f"STATUS: {ers[1]}")            # get the status code for the response.
+print(f"RESULTS: {results[0].results}")        # get the result data of the resource query.
+print(f"STATUS: {results[0].type}")            # get the status code for the response.
